a4ddb399f79ca0d0b76c29b0cb07bf1519561602
Jedffry Pieters <Jeffreyaugust.pieters@gmail.com>
2025-02-19 20:54:59 +0100

    Fixed main.cpp

--- CocktailDispensor/src/main.cpp
+++ CocktailDispensor/src/main.cpp
@@ -4,6 +4,14 @@
 #include <Liquor.h>
 #include <TaskHelper.h>
 #include <InterCoreHandler.h>
+#include <Bootloader.h>
+#include <WiFi.h>
+
+Bootloader* bootloader;
+
+static const char* ssid = "Thuis1";
+static const char* password = "Leesbril123";
+
 void TestLoop1(void* parameters)
 {
   Serial.print("Task 1 running on core ");
@@ -19,25 +27,7 @@ void TestLoop2(void* parameters)
 {
   Serial.print("Task 2 running on core ");
   Serial.println(xPortGetCoreID());
-
 }
-
-void setup() {
-  Serial.begin(115200);
-  std::string name = "test1";
-  TaskHelper task1(name, TestLoop1, 1000, NULL, 0, TASKHELPER_PRIO_MEDIUM);
-  TaskHelper task2("test2", TestLoop2, 1000, NULL, 0, TASKHELPER_PRIO_HIGH);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-#include <Bootloader.h>
-#include <WiFi.h>
-
-Bootloader* bootloader;
-
-static const char* ssid = "";
-static const char* password = "";
 
 void setup() 
 {
@@ -51,7 +41,9 @@ void setup()
 
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-
+  std::string name = "test1";
+  TaskHelper task1(name, TestLoop1, 1000, NULL, 0, TASKHELPER_PRIO_MEDIUM);
+  TaskHelper task2("test2", TestLoop2, 1000, NULL, 0, TASKHELPER_PRIO_HIGH);
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) 
   {
     Serial.println("STA Failed to configure");
@@ -68,3 +60,5 @@ void loop()
 {
   bootloader->UpdateFirmware();
 }
+
+
